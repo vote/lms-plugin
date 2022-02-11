843a027d618709b9e8d56181f5f052f5d4b74d0f
Aidan Feldman <aidan.feldman@gmail.com>
2022-02-11 15:55:30 -0500

    delete a bunch of unused example code

--- game/app.py
+++ game/app.py
@@ -1,16 +1,11 @@
-import datetime
 import os
 import pprint
 
 from tempfile import mkdtemp
-from flask import Flask, jsonify, request, render_template, url_for
+from flask import Flask, jsonify, request, render_template
 from flask_caching import Cache
 from flask_debugtoolbar import DebugToolbarExtension
-from werkzeug.exceptions import Forbidden
 from pylti1p3.contrib.flask import FlaskOIDCLogin, FlaskMessageLaunch, FlaskRequest, FlaskCacheDataStorage
-from pylti1p3.deep_link_resource import DeepLinkResource
-from pylti1p3.grade import Grade
-from pylti1p3.lineitem import LineItem
 from pylti1p3.tool_config import ToolConfJsonFile
 from pylti1p3.registration import Registration
 
@@ -128,140 +123,6 @@ def launch():
 def get_jwks():
     tool_conf = ToolConfJsonFile(get_lti_config_path())
     return jsonify({'keys': tool_conf.get_jwks()})
-
-
-@app.route('/configure/<launch_id>/<difficulty>/', methods=['GET', 'POST'])
-def configure(launch_id, difficulty):
-    tool_conf = ToolConfJsonFile(get_lti_config_path())
-    flask_request = FlaskRequest()
-    launch_data_storage = get_launch_data_storage()
-    message_launch = ExtendedFlaskMessageLaunch.from_cache(launch_id, flask_request, tool_conf,
-                                                           launch_data_storage=launch_data_storage)
-
-    if not message_launch.is_deep_link_launch():
-        raise Forbidden('Must be a deep link!')
-
-    launch_url = url_for('launch', _external=True)
-
-    resource = DeepLinkResource()
-    resource.set_url(launch_url + '?difficulty=' + difficulty) \
-        .set_custom_params({'difficulty': difficulty}) \
-        .set_title('Breakout ' + difficulty + ' mode!')
-
-    html = message_launch.get_deep_link().output_response_form([resource])
-    return html
-
-
-@app.route('/api/score/<launch_id>/<earned_score>/<time_spent>/', methods=['POST'])
-def score(launch_id, earned_score, time_spent):
-    tool_conf = ToolConfJsonFile(get_lti_config_path())
-    flask_request = FlaskRequest()
-    launch_data_storage = get_launch_data_storage()
-    message_launch = ExtendedFlaskMessageLaunch.from_cache(launch_id, flask_request, tool_conf,
-                                                           launch_data_storage=launch_data_storage)
-
-    resource_link_id = message_launch.get_launch_data() \
-        .get('https://purl.imsglobal.org/spec/lti/claim/resource_link', {}).get('id')
-
-    if not message_launch.has_ags():
-        raise Forbidden("Don't have grades!")
-
-    sub = message_launch.get_launch_data().get('sub')
-    timestamp = datetime.datetime.utcnow().isoformat() + 'Z'
-    earned_score = int(earned_score)
-    time_spent = int(time_spent)
-
-    grades = message_launch.get_ags()
-    sc = Grade()
-    sc.set_score_given(earned_score) \
-        .set_score_maximum(100) \
-        .set_timestamp(timestamp) \
-        .set_activity_progress('Completed') \
-        .set_grading_progress('FullyGraded') \
-        .set_user_id(sub)
-
-    sc_line_item = LineItem()
-    sc_line_item.set_tag('score') \
-        .set_score_maximum(100) \
-        .set_label('Score')
-    if resource_link_id:
-        sc_line_item.set_resource_id(resource_link_id)
-
-    grades.put_grade(sc, sc_line_item)
-
-    tm = Grade()
-    tm.set_score_given(time_spent) \
-        .set_score_maximum(999) \
-        .set_timestamp(timestamp) \
-        .set_activity_progress('Completed') \
-        .set_grading_progress('FullyGraded') \
-        .set_user_id(sub)
-
-    tm_line_item = LineItem()
-    tm_line_item.set_tag('time') \
-        .set_score_maximum(999) \
-        .set_label('Time Taken')
-    if resource_link_id:
-        tm_line_item.set_resource_id(resource_link_id)
-
-    result = grades.put_grade(tm, tm_line_item)
-
-    return jsonify({'success': True, 'result': result.get('body')})
-
-
-@app.route('/api/scoreboard/<launch_id>/', methods=['GET', 'POST'])
-def scoreboard(launch_id):
-    tool_conf = ToolConfJsonFile(get_lti_config_path())
-    flask_request = FlaskRequest()
-    launch_data_storage = get_launch_data_storage()
-    message_launch = ExtendedFlaskMessageLaunch.from_cache(launch_id, flask_request, tool_conf,
-                                                           launch_data_storage=launch_data_storage)
-
-    resource_link_id = message_launch.get_launch_data() \
-        .get('https://purl.imsglobal.org/spec/lti/claim/resource_link', {}).get('id')
-
-    if not message_launch.has_nrps():
-        raise Forbidden("Don't have names and roles!")
-
-    if not message_launch.has_ags():
-        raise Forbidden("Don't have grades!")
-
-    ags = message_launch.get_ags()
-
-    score_line_item = LineItem()
-    score_line_item.set_tag('score') \
-        .set_score_maximum(100) \
-        .set_label('Score')
-    if resource_link_id:
-        score_line_item.set_resource_id(resource_link_id)
-
-    scores = ags.get_grades(score_line_item)
-
-    time_line_item = LineItem()
-    time_line_item.set_tag('time') \
-        .set_score_maximum(999) \
-        .set_label('Time Taken')
-    if resource_link_id:
-        time_line_item.set_resource_id(resource_link_id)
-
-    times = ags.get_grades(time_line_item)
-
-    members = message_launch.get_nrps().get_members()
-    scoreboard_result = []
-
-    for sc in scores:
-        result = {'score': sc['resultScore']}
-        for tm in times:
-            if tm['userId'] == sc['userId']:
-                result['time'] = tm['resultScore']
-                break
-        for member in members:
-            if member['user_id'] == sc['userId']:
-                result['name'] = member.get('name', 'Unknown')
-                break
-        scoreboard_result.append(result)
-
-    return jsonify(scoreboard_result)
 
 
 if __name__ == '__main__':
