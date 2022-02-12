bf9b00e9be3eea5edef16607223bac08ded2d2ec
Aidan Feldman <aidan.feldman@gmail.com>
2022-02-12 00:57:01 -0500

    format files

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ from tempfile import mkdtemp
 from urllib.parse import urlencode
 from flask import Flask, jsonify, render_template
 from flask_caching import Cache
-from pylti1p3.contrib.flask import FlaskOIDCLogin, FlaskMessageLaunch, FlaskRequest, FlaskCacheDataStorage
+from pylti1p3.contrib.flask import (
+    FlaskOIDCLogin,
+    FlaskMessageLaunch,
+    FlaskRequest,
+    FlaskCacheDataStorage,
+)
 from pylti1p3.tool_config import ToolConfJsonFile
 from pylti1p3.registration import Registration
 from werkzeug.middleware.proxy_fix import ProxyFix
@@ -18,7 +23,7 @@ config = {
     "CACHE_DEFAULT_TIMEOUT": 600,
     "SECRET_KEY": "replace-me",
     "SESSION_TYPE": "filesystem",
-    "SESSION_FILE_DIR": mkdtemp()
+    "SESSION_FILE_DIR": mkdtemp(),
 }
 app.config.from_mapping(config)
 cache = Cache(app)
@@ -32,7 +37,7 @@ class ExtendedFlaskMessageLaunch(FlaskMessageLaunch):
 
 
 def get_lti_config_path():
-    return os.path.join(app.root_path, 'configs', 'lti.json')
+    return os.path.join(app.root_path, "configs", "lti.json")
 
 
 def get_launch_data_storage():
@@ -40,52 +45,56 @@ def get_launch_data_storage():
 
 
 def get_jwk_from_public_key(key_name):
-    key_path = os.path.join(app.root_path, 'configs', key_name)
-    f = open(key_path, 'r')
+    key_path = os.path.join(app.root_path, "configs", key_name)
+    f = open(key_path, "r")
     key_content = f.read()
     jwk = Registration.get_jwk(key_content)
     f.close()
     return jwk
 
 
-@app.route('/login/', methods=['GET', 'POST'])
+@app.route("/login/", methods=["GET", "POST"])
 def login():
     tool_conf = ToolConfJsonFile(get_lti_config_path())
     launch_data_storage = get_launch_data_storage()
 
     flask_request = FlaskRequest()
-    target_link_uri = flask_request.get_param('target_link_uri')
+    target_link_uri = flask_request.get_param("target_link_uri")
     if not target_link_uri:
         raise Exception('Missing "target_link_uri" param')
 
-    oidc_login = FlaskOIDCLogin(flask_request, tool_conf, launch_data_storage=launch_data_storage)
-    return oidc_login\
-        .enable_check_cookies()\
-        .redirect(target_link_uri)
+    oidc_login = FlaskOIDCLogin(
+        flask_request, tool_conf, launch_data_storage=launch_data_storage
+    )
+    return oidc_login.enable_check_cookies().redirect(target_link_uri)
 
 
-@app.route('/launch/', methods=['POST'])
+@app.route("/launch/", methods=["POST"])
 def launch():
     tool_conf = ToolConfJsonFile(get_lti_config_path())
     flask_request = FlaskRequest()
     launch_data_storage = get_launch_data_storage()
-    message_launch = ExtendedFlaskMessageLaunch(flask_request, tool_conf, launch_data_storage=launch_data_storage)
+    message_launch = ExtendedFlaskMessageLaunch(
+        flask_request, tool_conf, launch_data_storage=launch_data_storage
+    )
     message_launch_data = message_launch.get_launch_data()
 
-    query = urlencode({
-        'first_name': message_launch_data.get('given_name', ''),
-        'last_name': message_launch_data.get('family_name', ''),
-        'email': message_launch_data.get('email', ''),
-    })
-    return render_template('index.html', query=query)
+    query = urlencode(
+        {
+            "first_name": message_launch_data.get("given_name", ""),
+            "last_name": message_launch_data.get("family_name", ""),
+            "email": message_launch_data.get("email", ""),
+        }
+    )
+    return render_template("index.html", query=query)
 
 
-@app.route('/jwks/', methods=['GET'])
+@app.route("/jwks/", methods=["GET"])
 def get_jwks():
     tool_conf = ToolConfJsonFile(get_lti_config_path())
-    return jsonify({'keys': tool_conf.get_jwks()})
+    return jsonify({"keys": tool_conf.get_jwks()})
 
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 9001))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 9001))
+    app.run(host="0.0.0.0", port=port)
